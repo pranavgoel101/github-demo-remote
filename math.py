3e867843c62c34bdebaa9c1295d1574417cabbd6
pranavgoel101 <53399212+pranavgoel101@users.noreply.github.com>
2021-08-02 14:30:57 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,19 @@
+#Add Implementation
+def add(x,y):
+    return x+y
+    
+#Subtract Implementation    
+def subtract(x,y):
+    return x-y
+    
+#Multiplication Implementation    
+def multiply(x,y):
+    return x*y
+    
+#Divide Implementation    
+def divide(x,y):
+    if y==0
+        return DIVIDE_BY_0_ERROR
+    else
+        return x/y
+
